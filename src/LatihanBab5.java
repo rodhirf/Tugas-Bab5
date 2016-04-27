233103b990d1d85a059941b764ecf66d15c6ad58
rodhirf <rodhirobb@gmail.com>
2016-04-27 13:14:13 +0700

    membuat class bernama Perhitungan dan deklarasi variabel

--- src/LatihanBab5.java
+++ src/LatihanBab5.java
@@ -1,17 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
-    public static void main(String[] args) {
-        System.out.println("tess");
-        
-        
-    }
-    
-}
+public class Perhitungan {
+
+    int m, n, r, gcd, atas, bawah;
