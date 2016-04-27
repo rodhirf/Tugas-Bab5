127f6e1b417496b2f0e384f1aacad0a2c51d058a
rodhirf <rodhirobb@gmail.com>
2016-04-27 13:19:01 +0700

    membuat class dan deklarasi variabek

--- src/tugas/bab5/TugasBab5.java
+++ src/tugas/bab5/TugasBab5.java
@@ -1,21 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab5;
-
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab5 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+public class Labkomdas {
+final int a = 100000;
+final int b = 125000;
+final int c = 175000;
+int total;
