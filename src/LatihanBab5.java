79f9486375da406969aa8abb408b143d4ef01875
rodhirf <rodhirobb@gmail.com>
2016-04-27 13:18:16 +0700

    membuat main class dari class perhitungan

--- src/LatihanBab5.java
+++ src/LatihanBab5.java
@@ -41,4 +41,78 @@ public void Sederhana(int a, int b) {
 
 }
 
+class mainPerhitungan {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int pilih;
+
+        do {
+            System.out.println("Program Perhitungan");
+            System.out.println("1. Penjumlahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Pembagian");
+            System.out.println("5. Sederhanakan");
+            System.out.println("0. Exit");
+            System.out.print("Masukkan Pilihan : ");
+            pilih = inp.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.println("Penjumlahan");
+                    System.out.print("Masukkan Angka : ");
+                    int angka1 = inp.nextInt();
+                    System.out.print("Masukkan Angka : ");
+                    int angka2 = inp.nextInt();
+                    Perhitungan.hitungPenjumlahan(angka1, angka2);
+                    break;
+
+                case 2:
+                    System.out.println("Pengurangan");
+                    System.out.print("Masukkan Angka : ");
+                    angka1 = inp.nextInt();
+                    System.out.print("Masukkan Angka : ");
+                    angka2 = inp.nextInt();
+                    Perhitungan.hitungPengurangan(angka1, angka2);
+                    break;
+
+                case 3:
+                    Perhitungan kali = new Perhitungan();
+                    System.out.println("Perkalian");
+                    System.out.print("Masukkan Angka : ");
+                    angka1 = inp.nextInt();
+                    System.out.print("Masukkan Angka : ");
+                    angka2 = inp.nextInt();
+                    kali.hitungPerkalian(angka1, angka2);
+                    break;
+
+                case 4:
+                    Perhitungan bagi = new Perhitungan();
+                    System.out.println("Pembagian");
+                    System.out.print("Masukkan Angka : ");
+                    angka1 = inp.nextInt();
+                    System.out.print("Masukkan Angka : ");
+                    angka2 = inp.nextInt();
+                    bagi.hitungPembagian(angka1, angka2);
+                    break;
+
+                case 5:
+                    Perhitungan sederhana = new Perhitungan();
+                    System.out.print("Masukkan pembilang : ");
+                    int a = inp.nextInt();
+                    System.out.print("Masukkan penyebut : ");
+                    int b = inp.nextInt();
+                    sederhana.Sederhana(a, b);
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak ditemukan");
+
+            }
+        } while (pilih != 0);
+    }
+
+}
+
+
 
