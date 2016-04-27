2a8265567c0e73142eb5243587512defeac38c72
rodhirf <rodhirobb@gmail.com>
2016-04-27 13:15:23 +0700

    membuat method void
    membuat method void untuk menghitung penjumlahan,pengurangan,perkalian,
    dan pembagian

--- src/LatihanBab5.java
+++ src/LatihanBab5.java
@@ -1,3 +1,24 @@
 public class Perhitungan {
 
     int m, n, r, gcd, atas, bawah;
+
+public static void hitungPenjumlahan(int a, int b) {
+        int nilai = a + b;
+        System.out.println("Hasil Penjumlahannya adalah : " + nilai);
+    }
+
+    public static void hitungPengurangan(int a, int b) {
+        int nilai = a - b;
+        System.out.println("Hasil Pengurangannya Adalah : " + nilai);
+    }
+
+    public void hitungPerkalian(int a, int b) {
+        int nilai = a * b;
+        System.out.println("Hasil Perkaliannya Adalah = " + nilai);
+    }
+
+    public void hitungPembagian(int a, int b) {
+        double nilai = a / b;
+        System.out.println("Hasil Pembagiannya Adalah = " + nilai);
+    }
+
